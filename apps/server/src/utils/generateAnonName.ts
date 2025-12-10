393caa8290f55d30109599939fb2546eb8267fa0
Hrusi01 <hrusikeshkar01@gmail.com>
2025-12-10 22:23:33 +0530

    updated hash
    Signed-off-by: Hrusi01 <hrusikeshkar01@gmail.com>

--- apps/server/src/utils/generateAnonName.ts
+++ apps/server/src/utils/generateAnonName.ts
@@ -1,6 +1,6 @@
 import crypto from "crypto";
-import { env } from "@/envSchema"; // wherever you load env vars
 
+import { getAnonSecret } from "../anonSecret";
 const animals = [
   "Tiger", "Wolf", "Falcon", "Panther", "Phoenix", "Leopard", "Eagle", "Raven",
   "Cobra", "Viper", "Hawk", "Dragon", "Lynx", "Jaguar", "Shark", "Stallion",
@@ -17,7 +17,7 @@ const adjectives = [
 
 
 export function generateAnonName(userId: string, threadId: string): string {
-  const secret = env.ANON_SECRET_KEY || "A9fK2xP7QwL8Rt3ZmX7pQ2sT9vB4nR6HZ4r!Q8u#P2k@M6yNp7L3vR9Qw2Xy6BfT";
+  const secret = getAnonSecret();
   const seed = `${userId}-${threadId}`;
   const hash = crypto
     .createHmac("sha256", secret)
@@ -26,5 +26,6 @@ export function generateAnonName(userId: string, threadId: string): string {
   const adjIndex = parseInt(hash.substring(0, 8), 16) % adjectives.length;
   const animalIndex = parseInt(hash.substring(8, 16), 16) % animals.length;
   const num = parseInt(hash.substring(16, 20), 16) % 100;
+
   return `${adjectives[adjIndex]}${animals[animalIndex]}${num}`;
 }
